a65544f0e1f57712e8db54df997dc4a67d62e84a
ydb9607 <98730718+ydb9607@users.noreply.github.com>
2023-02-07 00:56:55 +0900

    Study Cook

--- Study Cook/script.js
+++ Study Cook/script.js
@@ -5,6 +5,25 @@ function showpopup () {
   
   function hidepopup () {
     var Popup = document.getElementById('popup')
-    Popup.classList.remove('show')
+    popup.classList.remove('hide');
   }
+
+  function showPopup(hasFilter) {
+    const popup = document.querySelector('#popup_2');
+    
+    if (hasFilter) {
+      popup.classList.add('has-filter');
+    } else {
+      popup.classList.remove('has-filter');
+    }
+    
+    popup.classList.remove('hide');
+  }
+  
+  function closePopup() {
+    const popup = document.querySelector('#popup_2');
+    popup.classList.add('hide');
+  }
+
+  
 
